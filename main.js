bf94d57bc7d1aa7bd59aef59036363c835d52320
JiningLiu <88249105+JiningLiu@users.noreply.github.com>
2021-09-30 17:47:56 -0500

    -5

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-
 $(document).scroll(function () {
     var y = $(this).scrollTop();
-    if (y > 200) {
+    if (y > 250) {
         $('.scroll1').fadeOut();
         $('.scroll2').fadeIn();
     } else {
