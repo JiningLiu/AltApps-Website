c7c39e9bbc2651e91ba15741d0041df168edca5d
Jining Liu <88249105+JiningLiu@users.noreply.github.com>
2021-10-01 20:02:01 +0000

    10/01-1 bug

--- main.js
+++ main.js
@@ -1,10 +1,48 @@
 $(document).scroll(function () {
     var y = $(this).scrollTop();
-    if (y > 250) {
+    if (y > 300) {
+      if (y < 600) {
         $('.scroll1').fadeOut();
         $('.scroll2').fadeIn();
+        $('.scroll3').fadeOut();
+        $('.scroll4').fadeOut();
+        $('.scroll5').fadeOut();
+      } else {
+        if (y > 600) {
+          if ($(window).width() > 1280)
+            $('.scroll1').fadeOut();
+            $('.scroll2').fadeOut();
+            $('.scroll3').fadeIn();
+            $('.scroll4').fadeOut();
+            $('.scroll5').fadeOut();
+          } else {
+            if (y < 900) {
+              $('.scroll1').fadeOut();
+              $('.scroll2').fadeOut();
+              $('.scroll3').fadeOut();
+              $('.scroll4').fadeIn();
+              $('.scroll5').fadeOut();
+            } else {
+              $('.scroll1').fadeOut();
+              $('.scroll2').fadeOut();
+              $('.scroll3').fadeOut();
+              $('.scroll4').fadeOut();
+              $('.scroll5').fadeIn();
+            }
+          }
+        } else {
+          $('.scroll1').fadeOut();
+          $('.scroll2').fadeIn();
+          $('.scroll3').fadeOut();
+          $('.scroll4').fadeOut();
+          $('.scroll5').fadeOut();
+        }
+      }
     } else {
-        $('.scroll2').fadeOut();
-        $('.scroll1').fadeIn();
+      $('.scroll1').fadeIn();
+      $('.scroll2').fadeOut();
+      $('.scroll3').fadeOut();
+      $('.scroll4').fadeOut();
+      $('.scroll5').fadeOut();
     }
 });
