21815a90606b23cc880db5561f40441bada4aae6
Jining Liu <88249105+JiningLiu@users.noreply.github.com>
2021-10-01 20:35:19 +0000

    10/01-2

--- main.js
+++ main.js
@@ -1,48 +1,31 @@
 $(document).scroll(function () {
-    var y = $(this).scrollTop();
-    if (y > 300) {
-      if (y < 600) {
+  var y = $(this).scrollTop();
+  if (y > 1000) {
+    if (y < 2000) {
+      $('.scroll1').fadeOut();
+      $('.scroll2').fadeIn();
+      $('.scroll3').fadeOut();
+      $('.scroll4').fadeOut();
+      $('.scroll5').fadeOut();
+    } else {
+      if (y > 3000) {
+        $('.scroll1').fadeOut();
+        $('.scroll2').fadeOut();
+        $('.scroll3').fadeIn();
+        $('.scroll4').fadeOut();
+        $('.scroll5').fadeOut();
+      } else {
         $('.scroll1').fadeOut();
         $('.scroll2').fadeIn();
         $('.scroll3').fadeOut();
         $('.scroll4').fadeOut();
-        $('.scroll5').fadeOut();
-      } else {
-        if (y > 600) {
-          if ($(window).width() > 1280)
-            $('.scroll1').fadeOut();
-            $('.scroll2').fadeOut();
-            $('.scroll3').fadeIn();
-            $('.scroll4').fadeOut();
-            $('.scroll5').fadeOut();
-          } else {
-            if (y < 900) {
-              $('.scroll1').fadeOut();
-              $('.scroll2').fadeOut();
-              $('.scroll3').fadeOut();
-              $('.scroll4').fadeIn();
-              $('.scroll5').fadeOut();
-            } else {
-              $('.scroll1').fadeOut();
-              $('.scroll2').fadeOut();
-              $('.scroll3').fadeOut();
-              $('.scroll4').fadeOut();
-              $('.scroll5').fadeIn();
-            }
-          }
-        } else {
-          $('.scroll1').fadeOut();
-          $('.scroll2').fadeIn();
-          $('.scroll3').fadeOut();
-          $('.scroll4').fadeOut();
-          $('.scroll5').fadeOut();
-        }
       }
-    } else {
-      $('.scroll1').fadeIn();
-      $('.scroll2').fadeOut();
-      $('.scroll3').fadeOut();
-      $('.scroll4').fadeOut();
-      $('.scroll5').fadeOut();
     }
+  } else {
+    $('.scroll1').fadeIn();
+    $('.scroll2').fadeOut();
+    $('.scroll3').fadeOut();
+    $('.scroll4').fadeOut();
+    $('.scroll5').fadeOut();
+  }
 });
