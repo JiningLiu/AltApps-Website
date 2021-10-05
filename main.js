b8c6228864c84df5de3a6c378646da69c3da1e65
Jining Liu <88249105+JiningLiu@users.noreply.github.com>
2021-10-05 19:28:56 +0000

    10/05-1

--- main.js
+++ main.js
@@ -2,83 +2,83 @@ $(document).scroll(function () {
   var y = $(this).scrollTop();
   if (y > 1000) {
     if (y < 2000) {
-      $('.scroll1').fadeOut();
-      $('.scroll2').fadeIn();
-      $('.scroll3').fadeOut();
-      $('.scroll4').fadeOut();
-      $('.scroll5').fadeOut();
-      $('.scroll6').fadeOut();
-      $('.scroll7').fadeOut();
-      $('.toTop').fadeIn();
-      $('.scrollDown').fadeOut();
+      $('.scroll1').fadeOut(250);
+      $('.scroll2').fadeIn(250);
+      $('.scroll3').fadeOut(250);
+      $('.scroll4').fadeOut(250);
+      $('.scroll5').fadeOut(250);
+      $('.scroll6').fadeOut(250);
+      $('.scroll7').fadeOut(250);
+      $('.toTop').fadeIn(250);
+      $('.scrollDown').fadeOut(250);
     } else {
       if (y > 3000) {
         if (y < 4000) {
-          $('.scroll1').fadeOut();
-          $('.scroll2').fadeOut();
-          $('.scroll3').fadeOut();
-          $('.scroll4').fadeIn();
-          $('.scroll5').fadeOut();
-          $('.scroll6').fadeOut();
-          $('.scroll7').fadeOut();
-          $('.toTop').fadeIn();
-          $('.scrollDown').fadeOut();
+          $('.scroll1').fadeOut(250);
+          $('.scroll2').fadeOut(250);
+          $('.scroll3').fadeOut(250);
+          $('.scroll4').fadeIn(250);
+          $('.scroll5').fadeOut(250);
+          $('.scroll6').fadeOut(250);
+          $('.scroll7').fadeOut(250);
+          $('.toTop').fadeIn(250);
+          $('.scrollDown').fadeOut(250);
         } else {
           if (y < 5000) {
-            $('.scroll1').fadeOut();
-            $('.scroll2').fadeOut();
-            $('.scroll3').fadeOut();
-            $('.scroll4').fadeOut();
-            $('.scroll5').fadeIn();
-            $('.scroll6').fadeOut();
-            $('.scroll7').fadeOut();
-            $('.toTop').fadeIn();
-            $('.scrollDown').fadeOut();
+            $('.scroll1').fadeOut(250);
+            $('.scroll2').fadeOut(250);
+            $('.scroll3').fadeOut(250);
+            $('.scroll4').fadeOut(250);
+            $('.scroll5').fadeIn(250);
+            $('.scroll6').fadeOut(250);
+            $('.scroll7').fadeOut(250);
+            $('.toTop').fadeIn(250);
+            $('.scrollDown').fadeOut(250);
           } else {
             if (y < 6000) {
-              $('.scroll1').fadeOut();
-              $('.scroll2').fadeOut();
-              $('.scroll3').fadeOut();
-              $('.scroll4').fadeOut();
-              $('.scroll5').fadeOut();
-              $('.scroll6').fadeIn();
-              $('.scroll7').fadeOut();
-              $('.toTop').fadeIn();
-              $('.scrollDown').fadeOut();
+              $('.scroll1').fadeOut(250);
+              $('.scroll2').fadeOut(250);
+              $('.scroll3').fadeOut(250);
+              $('.scroll4').fadeOut(250);
+              $('.scroll5').fadeOut(250);
+              $('.scroll6').fadeIn(250);
+              $('.scroll7').fadeOut(250);
+              $('.toTop').fadeIn(250);
+              $('.scrollDown').fadeOut(250);
             } else {
-              $('.scroll1').fadeOut();
-              $('.scroll2').fadeOut();
-              $('.scroll3').fadeOut();
-              $('.scroll4').fadeOut();
-              $('.scroll5').fadeOut();
-              $('.scroll6').fadeOut();
-              $('.scroll7').fadeIn();
-              $('.toTop').fadeIn();
-              $('.scrollDown').fadeOut();
+              $('.scroll1').fadeOut(250);
+              $('.scroll2').fadeOut(250);
+              $('.scroll3').fadeOut(250);
+              $('.scroll4').fadeOut(250);
+              $('.scroll5').fadeOut(250);
+              $('.scroll6').fadeOut(250);
+              $('.scroll7').fadeIn(250);
+              $('.toTop').fadeIn(250);
+              $('.scrollDown').fadeOut(250);
             }
           }
         }
       } else {
-        $('.scroll1').fadeOut();
-        $('.scroll2').fadeOut();
-        $('.scroll3').fadeIn();
-        $('.scroll4').fadeOut();
-        $('.scroll5').fadeOut();
-        $('.scroll6').fadeOut();
-        $('.scroll7').fadeOut();
-        $('.toTop').fadeIn();
-        $('.scrollDown').fadeOut();
+        $('.scroll1').fadeOut(250);
+        $('.scroll2').fadeOut(250);
+        $('.scroll3').fadeIn(250);
+        $('.scroll4').fadeOut(250);
+        $('.scroll5').fadeOut(250);
+        $('.scroll6').fadeOut(250);
+        $('.scroll7').fadeOut(250);
+        $('.toTop').fadeIn(250);
+        $('.scrollDown').fadeOut(250);
       }
     }
   } else {
-    $('.scroll1').fadeIn();
-    $('.scroll2').fadeOut();
-    $('.scroll3').fadeOut();
-    $('.scroll4').fadeOut();
-    $('.scroll5').fadeOut();
-    $('.scroll6').fadeOut();
-    $('.scroll7').fadeOut();
-    $('.toTop').fadeOut();
-    $('.scrollDown').fadeIn();
+    $('.scroll1').fadeIn(250);
+    $('.scroll2').fadeOut(250);
+    $('.scroll3').fadeOut(250);
+    $('.scroll4').fadeOut(250);
+    $('.scroll5').fadeOut(250);
+    $('.scroll6').fadeOut(250);
+    $('.scroll7').fadeOut(250);
+    $('.toTop').fadeOut(250);
+    $('.scrollDown').fadeIn(250);
   }
 });
